7983edf8b38f2248be22d00573fd1b2c22b008c8
Olive 3 <141564336+vOlive-io@users.noreply.github.com>
2024-04-18 14:12:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -89,6 +89,17 @@ function updateGains() {
 }
 
 function updatePoints() {
+    legacy++;
+    let displayPoints = legacy;
+  for (let i = suffixes.length - 1; i >= 1; i--) {
+    const limit = Math.pow(10, i * 3);
+    if (legacy >= limit) {
+      displayPoints = (legacy / limit).toFixed(1) + suffixes[i];
+      break;
+    }
+  }
+    circle.innerHTML = "😈 - " + displayPoints;
+}
 }
 
 ////////////////////////////////
