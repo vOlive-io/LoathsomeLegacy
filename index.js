8ad1633df8ffad0af07d93b2f11ab7fcfe1190b2
Olive 3 <141564336+vOlive-io@users.noreply.github.com>
2024-04-22 10:24:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var clickerCost = 15;
 var orbCost = 100;
 
 //light auto
-var baseClickerGain = 0.0005;;
-var baseOrbGain = 0.005;
+var baseClickerGain = 0.0001;;
+var baseOrbGain = 0.0025;
 var baseFarmGain = 0.05;
 var baseMineGain = 50;
 var baseFactoryGain = 1;
