19578c60c98daf9069dff15265b55caae3191232
Olive 3 <141564336+vOlive-io@users.noreply.github.com>
2024-04-23 12:04:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -170,3 +170,19 @@ function autogain() {
 
 
 /////////////////////////////////////
+function 
+if (legacy < 1000) {
+    let roundlegacy = Math.round(legacy);
+    circle.innerText = "😈 - " + roundCount;
+  } else {
+ let displayPoints = legacy;
+  for (let i = suffixes.length - 1; i >= 1; i--) {
+    const limit = Math.pow(10, i * 3);
+    if (legacy >= limit) {
+      displayPoints = (legacy / limit).toFixed(1) + suffixes[i];
+      break;
+    }
+  }
+    circle.innerHTML = "😈 - " + displayPoints;
+  }
+}
